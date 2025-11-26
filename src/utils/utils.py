6bb85d4f6288bba94cf93778853b1df7f209487f
Timoteo <timoteo@hdinfo-ma.com>
2025-11-26 14:38:13 -0300

    main: Implementado funções de validar email e telefone.

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,5 +1,36 @@
-def validar_email():
-    pass
+from email_validator import validate_email, EmailNotValidError
+import phonenumbers
 
-def validar_telefone():
-    pass
+def validar_email(email: str):
+    try:
+        email_valido = validate_email(email)
+        print(email_valido)
+        if email_valido:
+            return email
+    
+    except EmailNotValidError as e:
+        print(e)
+        return e
+
+def validar_telefone(telefone: str, ddd_default="11"):
+    telefone = telefone.strip()
+
+    # remove espaços, parenteses e traços
+    digits = "".join(filter(str.isdigit, telefone))
+
+    # se só vier número de celular (9 dígitos), adiciona DDD padrão
+    if len(digits) == 9:
+        digits = ddd_default + digits
+
+    try:
+        parsed = phonenumbers.parse(digits, "BR")
+
+        if phonenumbers.is_valid_number(parsed):
+            return phonenumbers.format_number(
+                parsed, phonenumbers.PhoneNumberFormat.NATIONAL
+            )
+
+        return "Número inválido."
+
+    except phonenumbers.NumberParseException as e:
+        return f"Erro: {e}"
