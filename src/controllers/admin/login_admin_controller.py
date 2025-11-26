5425b6c9a39d59347e715a7317930799619ca4e9
Timoteo <timoteo@hdinfo-ma.com>
2025-11-26 10:18:43 -0300

    main: Implementação da tela index.html e rota de incio.

--- src/controllers/admin/login_admin_controller.py
+++ src/controllers/admin/login_admin_controller.py
@@ -8,6 +8,10 @@ from flask import (
 admin_bp = Blueprint("admin", __name__, url_prefix="/admin")
 
 
+@admin_bp.route("/", methods=["GET", "POST"])
+def index_page():
+    return render_template("index.html")
+
 @admin_bp.route("/login", methods=["GET", "POST"])
 def login_admin():
     return render_template("login_adm.html")
