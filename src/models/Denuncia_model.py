cfd5d9ab7ccc309bbbb771c8c8884f0eb738ca23
Timoteo <timoteo@hdinfo-ma.com>
2025-11-28 14:49:21 -0300

    main: adicionar coluna de nomes de testemunhas na tabela denuncias.

--- src/models/Denuncia_model.py
+++ src/models/Denuncia_model.py
@@ -9,6 +9,7 @@ class Denuncia(db.Model):
     frequencia = db.Column(db.String(255), nullable=False)
     data = db.Column(db.Date, nullable=False)
     testemunha = db.Column(db.String(10), nullable=False)
+    nomes_testemunhas = db.Column(db.Text(), nullable=True)
     descricao_do_fato = db.Column(db.Text, nullable=False)
     
     vitima_id = db.Column(db.Integer, db.ForeignKey("vitimas.id"), nullable=False)
@@ -17,11 +18,12 @@ class Denuncia(db.Model):
     vitima = db.relationship("Vitima", back_populates="denuncias")
     ofesor = db.relationship("Ofesor", back_populates="denuncias")
     
-    def __init__(self, categoria, frequencia, data, testemunha, descricao_do_fato):
+    def __init__(self, categoria, frequencia, data, testemunha, nomes_testemunhas, descricao_do_fato):
         self.categoria = categoria
         self.frequencia = frequencia
         self.data = data
         self.testemunha = testemunha
+        self.nomes_testemunhas = nomes_testemunhas
         self.descricao_do_fato = descricao_do_fato
         
     def __repr__(self):
